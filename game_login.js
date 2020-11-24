5e608165cf863922e680e1e94dc9b8008a5c1b2a
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2020-11-24 19:50:39 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -5,3 +5,26 @@ function addUser() {
     localStorage.setItem("player2_name",player2_name);
     window.location.replace("game_page.html");
 }
+function send() {
+    get_word=document.getElementById("word").value;
+    word=get_word.toLowerCase();
+    console.log("Word in lower case="+word);
+    charAt1=word.charAt(1);
+    console.log(charAt1);
+    lenth_divide_2=Math.floor(word.length/2);
+    charAt2=word.charAt(lenth_divide_2);
+    console.log(charAt2);
+    lenth_minus_1=word.length-1;
+    charAt3=word.charAt(lenth_minus_1);
+    console.log(charAt3);
+    remove_charAt1=word.replace(charAt1,"_");
+    remove_charAt2=remove_charAt1.replace(charAt2,"_");
+    remove_charAt3=remove_charAt2.replace(charAt3,"_");
+    console.log(remove_charAt3);
+    question_word="<h4 id='word_display'>Q."+remove_charAt3+"</h4>";
+    input_box="<br>Answer: <input type='text' id='input_check_box'>";
+    check_button="<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row=question_word+input_box+check_button;
+    document.getElementById("output").innerHTML=row;
+    document.getElementById("word").value="";
+}
